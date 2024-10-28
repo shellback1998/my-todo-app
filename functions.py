f408ae2cdb6ffe8b7d433d66198cafd80e114e77
Scott Gaudet <scott.gaudet@ge.com>
2024-10-28 16:11:23 -0400

    Initial commit

---
+++ functions.py
@@ -0,0 +1,20 @@
+FILEPATH = "C:/Users/223141708/PycharmProjects/todos/Python Mega Course/app1/todofiles/todos.txt"
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list of
+    to-do itesms.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath_arg=FILEPATH):
+    """ Write the to-do items list in the text file."""
+    with open(filepath_arg, 'w') as file_local:
+        file_local.writelines(todos_arg)
+
+#
+# if __name__ == "__main__":
+#     print("Hello")
+#     print(get_todos())
